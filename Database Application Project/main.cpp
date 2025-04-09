deafbc4b3fb3ec003ab4805993e8a51c9c786bf0
Voss <joshvosslerwork@gmail.com>
2025-04-08 21:25:35 -0400

    -boiler plate for sfml window added -added a button with text and a if statement that detects when the button is pressed

--- Database Application Project/main.cpp
+++ Database Application Project/main.cpp
@@ -1,23 +1,58 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
+using namespace sf;
+using namespace std;
+
 int main()
 {
-	sf::RenderWindow* window = new sf::RenderWindow(sf::VideoMode({ 1280,720 }), "Test");
-	window->setFramerateLimit(60);
+	RenderWindow window = RenderWindow(VideoMode({ 1280,720 }), "Test");
+	window.setFramerateLimit(60);
 
-	while (window->isOpen())
+	Font font;
+	
+	if (!font.openFromFile("arial.ttf"))
 	{
-		while (const std::optional event = window->pollEvent())
+		cout << "invalid location/name";
+	}
+
+	Text text(font);
+
+	text.setString("Button");
+	text.setCharacterSize(24);
+	text.setFillColor(Color::Red);
+	text.setOrigin({ text.getGlobalBounds().getCenter().x, text.getGlobalBounds().getCenter().y});
+	text.setPosition({640,360});
+
+	RectangleShape button({75,50});
+	button.setOrigin({button.getGeometricCenter().x, button.getGeometricCenter().y});
+	button.setPosition({ 640,360 });
+
+	while (window.isOpen())
+	{
+		while (const optional event = window.pollEvent())
 		{
-			if (event->is < sf::Event::Closed>())
+			if (event->is < Event::Closed>())
 			{
-				window->close();
+				window.close();
 			}
 		}
 
-		window->clear();
-		window->display();
+		if (Mouse::getPosition(window).x > (button.getPosition().x - button.getGeometricCenter().x)
+			&& Mouse::getPosition(window).x < (button.getPosition().x + button.getGeometricCenter().x)
+			&& Mouse::getPosition(window).y >(button.getPosition().y - button.getGeometricCenter().y)
+			&& Mouse::getPosition(window).y < (button.getPosition().y + button.getGeometricCenter().y))
+		{
+			if (Mouse::isButtonPressed(Mouse::Button::Left))
+			{
+				cout << "Button clicked" << endl;
+			}
+		}
+
+		window.clear();
+		window.draw(button);
+		window.draw(text);
+		window.display();
 	}
 
 
